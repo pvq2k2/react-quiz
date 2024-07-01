efdbcaff9676dde7e6cac494dfd6516f7e885ace
pvq2k2 <vanquyet2392002@gmail.com>
2024-07-02 00:58:36 +0700

    design home page

--- src/components/Home/HomePage.jsx
+++ src/components/Home/HomePage.jsx
@@ -1,7 +1,13 @@
 import React from "react";
-
+import videoHomepage from "../../assets/video-homepage.mp4";
 const HomePage = () => {
-  return <div className="homepage-container">HomePage</div>;
+  return (
+    <div className="homepage-container">
+      <video autoPlay muted loop>
+        <source src={videoHomepage} type="video/mp4" />
+      </video>
+    </div>
+  );
 };
 
 export default HomePage;
