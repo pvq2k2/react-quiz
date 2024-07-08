50dec741f667ee8a385ea3876b5bbe418bfacc0b
pvq2k2 <vanquyet2392002@gmail.com>
2024-07-08 14:17:20 +0700

    #83 process data quiz

--- src/components/User/DetailQuiz.jsx
+++ src/components/User/DetailQuiz.jsx
@@ -2,7 +2,8 @@
 import React, { useEffect } from "react";
 import { useParams } from "react-router-dom";
 import { getDataQuiz } from "../../services/apiService";
-
+import _ from "lodash";
+import { toast } from "react-toastify";
 const DetailQuiz = () => {
   const params = useParams();
   const quizId = params.id;
@@ -12,7 +13,28 @@ const DetailQuiz = () => {
   }, [quizId]);
   const fetchQuestions = async () => {
     const res = await getDataQuiz(quizId);
-    console.log(res);
+    if (res && res.EC === 0) {
+      let raw = res.DT;
+      let data = _.chain(raw)
+        .groupBy("id")
+        .map((value, key) => {
+          let answers = [];
+          let questionDescription,
+            image = null;
+          value.forEach((item, index) => {
+            if (index === 0) {
+              questionDescription = item.description;
+              image = item.image;
+            }
+            answers.push(item.answers);
+          });
+          return { questionId: key, answers, questionDescription, image };
+        })
+        .value();
+      console.log(data);
+    } else {
+      toast.error(res.EM);
+    }
   };
   return <div className="detail-quiz-container">DetailQuiz</div>;
 };
